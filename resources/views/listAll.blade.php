c4e911c0e4ea726575a15e71129b4f45741e4e83
dompossebon <dompossebon@gmail.com>
2020-02-12 16:33:18 -0300

    detalhe controller - II

--- resources/views/listAll.blade.php
+++ resources/views/listAll.blade.php
@@ -5,9 +5,9 @@
         <div class="row justify-content-center">
             <div class="card">
                 <div class="card-header">
-                    @if(session()->has('message'))
+                    @if(session()->has('success'))
                         <div class="alert alert-success">
-                            {{ session()->get('message') }}
+                            {{ session()->get('success') }}
                         </div>
                     @elseif(session()->has('danger'))
                             <div class="alert alert-danger">
